8448280510b9ffc14af4bca4bab15bfa702e8f52
Damian Ziobro <damian@xmementoit.com>
2015-08-27 23:29:54 +0100

    update installVim.sh

--- installVim.sh
+++ installVim.sh
@@ -5,15 +5,22 @@ VIM_ROOT=$HOME/.vim
 RANDOM_NR=`shuf -i 1000-90000 -n 1`
 INSTALL_ROOT=`pwd`
 
-if [ -d $VIM_ROOT ]; then 
+if [ -d $VIM_ROOT -o -L $VIM_ROOT ]; then 
     mv $VIM_ROOT $HOME/.vim_$RANDOM_NR
 fi 
 
 cd 
-mv $INSTALL_ROOT $VIM_ROOT
-cd $VIM_ROOT
-if [ -f $HOME/.vimrc ]; then 
-    rm $HOME/.vimrc
+#checking whether install from vim-ide dir or dotfiles
+BASE_INSTALL_DIR=`basename $INSTALL_ROOT`
+if [ $BASE_INSTALL_DIR == "vim-ide" ];
+then
+    mv $INSTALL_ROOT $VIM_ROOT
+else 
+    ln -s $INSTALL_ROOT $VIM_ROOT
+fi
+
+if [ -f $HOME/.vimrc -o -L $HOME/.vimrc ]; then 
+    mv $HOME/.vimrc $HOME/.vimrc_$RANDOM_NR
 fi
 
 ln -s $VIM_ROOT/vimrc $HOME/.vimrc
@@ -29,6 +36,7 @@ echo -e "Installing ctags"
 sudo apt-get install cscope
 
 echo -e "Initializing and checking out plugins submodules: "
+
 cd $VIM_ROOT 
 git submodule init 
 git submodule update 
